6f2d70b1dc92a39f033890412fac3414981cac89
Alexey Naumov <alexey.naumov@chimpchange.me>
2017-01-30 20:02:42 +0600

    Add application target in order to make responder chain working

---
+++ CallbackTechniques/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  CallbackTechniques
+//
+//  Created by Alexey Naumov on 30/01/2017.
+//  Copyright © 2017 Alexey Naumov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+  
+}
+
