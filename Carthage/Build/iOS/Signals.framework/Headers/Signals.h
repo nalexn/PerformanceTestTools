77ebd50712d79009252a68426e304b1c91b9f17b
Alexey Naumov <alexey.naumov@chimpchange.me>
2017-02-11 03:34:10 +0600

    Add Signals framework

---
+++ Carthage/Build/iOS/Signals.framework/Headers/Signals.h
@@ -0,0 +1,15 @@
+//
+//  Signals.h
+//  Signals
+//
+//  Created by Tuomas Artman on 16.8.2014.
+//  Copyright (c) 2014 Tuomas Artman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Signals.
+FOUNDATION_EXPORT double SignalsVersionNumber;
+
+//! Project version string for Signals.
+FOUNDATION_EXPORT const unsigned char SignalsVersionString[];
